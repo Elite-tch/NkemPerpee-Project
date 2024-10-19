f0e4b78c785bec06d624c6e348031c907a834c0d
Elite-tch <elite6461@gmail.com>
2024-10-19 11:17:09 +0100

    Commit message

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Hero } from '../../component/hero/hero'
+import { Hero } from '../../component/hero/Hero'
 import { Services } from '../../component/services/Services'
 
 export const LandingPage = () => {
   return (
     <div>
-        <Hero/>
-        <Services/>
+        <Hero />
+        <Services />
     </div>
   )
 }
