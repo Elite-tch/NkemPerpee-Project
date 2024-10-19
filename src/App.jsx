84852c123878596b70044ff1aa81fa340d77ecdc
Elite-tch <elite6461@gmail.com>
2024-10-19 11:02:53 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import Experience from "./component/experience"
+import Experience from "./component/experience";
 import './App.css';
 import './index.css';
 import { LandingPage } from './pages/LandingPage/LandingPage';
+
 function App() {
 
 
   return (
-<<<<<<< HEAD
     <div>
+  <LandingPage />    
 <Experience />
 
     </div>
-=======
-   <LandingPage/>
->>>>>>> 4746d02e9ba2af288c2815a2f18ea647559ebc22
   )
 }
 
